96c6ad69ade5f9ad9975087813e42b2749279f51
rihanuch <rihanuch@uc.cl>
2019-07-08 17:30:52 -0400

    added delete method

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('new_activity/', views.NewActivity.as_view(), name='new_activity'),
+    path('new/', views.NewActivity.as_view(), name='new_activity'),
+    path('', views.ActivitiesList.as_view(), name='activities_list'),
+    re_path(r'^delete/(?P<pk>\d+)/$', views.ActivityDelte.as_view(), name='delete_activity'),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,19 +1,49 @@
-from .forms import ActivityFrom
+from activities.forms import ActivityFrom
+from activities.models import Activity
+from accounts.models import Profile
 from django.views import generic
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+
+# limita permisos para el usuario creador
+class PermissionMixin(object):
+    def get_object(self, *args, **kwargs):
+        obj = super(PermissionMixin, self).get_object(*args, **kwargs)
+        if not obj.creator.id == self.request.user.id:
+            raise PermissionDenied()
+        else:
+            return obj
 
 
 # LoginRequiredMixin acts as a decorator to make a login required
 # for certain pages
-class NewActivity(LoginRequiredMixin, generic.CreateView):
+class NewActivity(LoginRequiredMixin, PermissionMixin, generic.CreateView):
     def get(self, request):
-        template = loader.get_template('new_activity.html')
+        template = loader.get_template('activities/new_activity.html')
         form = ActivityFrom(request.user)
         return HttpResponse(template.render({'form': form}, request))
 
     form_class = ActivityFrom
     success_url = reverse_lazy('home')
-    template_name = 'new_activity.html'
+    template_name = 'activities/new_activity.html'
+
+
+class ActivitiesList(LoginRequiredMixin, generic.ListView):
+    def get_queryset(self):
+        creator = Profile.objects.get(pk=self.request.user.id)
+        queryset = Activity.objects.filter(creator=creator)
+        return queryset
+
+    template_name = 'activities/activities_list.html'
+    model = Activity
+
+
+class ActivityDelte(LoginRequiredMixin, PermissionMixin, generic.DeleteView):
+
+    model = Activity
+    template_name = 'activities/activity_confirm_delete.html'
+    success_url = reverse_lazy('activities_list')
