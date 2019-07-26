f7f2ae33ffeff47a73b48f9ae7e106fb51807570
rihanuch <rihanuch@uc.cl>
2019-07-26 16:52:19 -0400

    added bulma

--- cai/settings.py
+++ cai/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'activities',
+    'bulma',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # login
 LOGIN_REDIRECT_URL = 'home'
