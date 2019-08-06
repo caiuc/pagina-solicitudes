2e25923895e6c123d4be60b9a0c645a94009ff51
rihanuch <rihanuch@uc.cl>
2019-08-06 18:20:40 -0400

    test sendgrid email

--- cai/settings.py
+++ cai/settings.py
@@ -139,12 +139,14 @@ LOGIN_REDIRECT_URL = 'activities_list'
 LOGOUT_REDIRECT_URL = 'activities_list'
 
 # emails
+EMAIL_BACKEND = 'sgbackend.SendGridBackend'
+
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
