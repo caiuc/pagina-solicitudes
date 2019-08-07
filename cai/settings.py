cde43e059975281080028406fa29479f6bd395e8
rihanuch <rihanuch@uc.cl>
2019-08-06 21:57:21 -0400

    second try gmail

--- cai/settings.py
+++ cai/settings.py
@@ -141,10 +141,10 @@ LOGOUT_REDIRECT_URL = 'activities_list'
 # emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 django_heroku.settings(locals())
