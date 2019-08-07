c39fc8d936d35b09f7e6894ca61ddf8057958f69
rihanuch <rihanuch@uc.cl>
2019-08-07 08:14:21 -0400

    try with sendgrid

--- cai/settings.py
+++ cai/settings.py
@@ -141,10 +141,10 @@ LOGOUT_REDIRECT_URL = 'activities_list'
 # emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 # SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
