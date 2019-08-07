76aeeebae27f5f8f18b21414d4f752ecabee7631
rihanuch <rihanuch@uc.cl>
2019-08-06 22:31:44 -0400

    try with gmail

--- cai/settings.py
+++ cai/settings.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 django_heroku.settings(locals())
