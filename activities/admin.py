10124d1b1fa0690f1736f9adc3edffb188db06a3
rihanuch <rihanuch@uc.cl>
2020-01-17 12:47:14 -0300

    added coordinator

--- activities/admin.py
+++ activities/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import (Space, Equipment)
+from .models import (Space, Equipment, Coordinator)
 
 admin.site.register(Space)
 admin.site.register(Equipment)
+admin.site.register(Coordinator)
