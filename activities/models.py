0c0a137427418bfb2d8c7b727cc1dd988cd85b05
Agustín Covarrubias <agucova@uc.cl>
2023-11-27 12:39:22 -0300

    feat: move to poetry, fix models

--- activities/models.py
+++ activities/models.py
@@ -1,121 +1,100 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+AUTH_USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
 
 
 class Space(models.Model):
-    """Physical space of activities
-    """
+    """Physical space of activities"""
+
+    name = models.CharField(max_length=200, verbose_name="Nombre de espacio")
+    description = models.CharField(max_length=2000, blank=True, null=False, verbose_name="Descripción de espacio")
+    admin_required = models.BooleanField(verbose_name="¿Requiere de link de administración?")
+    color = models.CharField(max_length=20, default="rgb(0,0,0)")
+    objects = models.Manager()
 
     class Meta:
-        verbose_name = 'Espacio'
-        verbose_name_plural = 'Espacios'
-
-    name = models.CharField(max_length=200, verbose_name='Nombre de espacio')
-    description = models.CharField(max_length=2000,
-                                   blank=True,
-                                   null=True,
-                                   verbose_name='Descripción de espacio')
-    admin_required = models.BooleanField(
-        verbose_name='¿Requiere de link de administración?')
-    color = models.CharField(max_length=20, default='rgb(0,0,0)')
-    objects = models.Manager()
+        verbose_name = "Espacio"
+        verbose_name_plural = "Espacios"
 
     def __str__(self):
         return self.name
 
 
 class Equipment(models.Model):
-    """Equipment to use in activities
-    """
+    """Equipment to use in activities"""
+
+    name = models.CharField(max_length=200, verbose_name="Nombre de equipamiento")
+    objects = models.Manager()
 
     class Meta:
-        verbose_name = 'Equipamiento'
-        verbose_name_plural = 'Equipamientos'
-
-    name = models.CharField(max_length=200,
-                            verbose_name='Nombre de equipamiento')
-    objects = models.Manager()
+        verbose_name = "Equipamiento"
+        verbose_name_plural = "Equipamientos"
 
     def __str__(self):
         return self.name
 
 
 class Activity(models.Model):
-    """Space request of a space and objects
-    """
+    """Space request of a space and objects"""
+
+    APPROVED = "A"
+    REJECTED = "R"
+    PENDING = "P"
+    CANCELLED = "C"
+    PENDING_CAI = "PC"
+    PENDING_ADMIN = "PA"
+
+    VALID_STATES = (
+        (APPROVED, "Aprobada"),
+        (REJECTED, "Rechazada"),
+        (PENDING, "Pendiente"),
+        (CANCELLED, "Cancelada"),
+        (PENDING_ADMIN, "En revisión por Decanato"),
+        (PENDING_CAI, "En revisión por CAi"),
+    )
+
+    state = models.CharField(
+        max_length=2,
+        choices=VALID_STATES,
+        default=PENDING_CAI,
+        verbose_name="Estado de la solicitud",
+    )
+    name = models.CharField(max_length=200, verbose_name="Nombre de la actividad")
+    description = models.TextField(max_length=20000, verbose_name="Descripción de la actividad")
+    space_1 = models.ForeignKey(Space, blank=True, null=True, on_delete=models.CASCADE, related_name="space_1")
+    space_2 = models.ForeignKey(Space, blank=True, null=True, on_delete=models.CASCADE, related_name="space_2")
+    space_3 = models.ForeignKey(Space, blank=True, null=True, on_delete=models.CASCADE, related_name="space_3")
+    equipment = models.ForeignKey(Equipment, blank=True, null=True, on_delete=models.CASCADE)
+    date_start = models.DateTimeField(blank=False, verbose_name="Fecha de inicio")
+    date_finish = models.DateTimeField(blank=False, verbose_name="Fecha de término")
+    in_charge = models.EmailField(max_length=200, verbose_name="Correo de encargado")
+    participants_amount = models.PositiveIntegerField(default=0, verbose_name="Número de participantes")
+    admin_link = models.URLField(blank=True, null=False, verbose_name="Link a administración del campus")
+    creator = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'Actividad'
-        verbose_name_plural = 'Actividades'
+        verbose_name = "Actividad"
+        verbose_name_plural = "Actividades"
 
-    APPROVED = 'A'
-    REJECTED = 'R'
-    PENDING = 'P'
-    CANCELLED = 'C'
-    PENDING_CAI = 'PC'
-    PENDING_ADMIN = 'PA'
-
-    VALID_STATES = ((APPROVED, 'Aprobada'), (REJECTED, 'Rechazada'),
-                    (PENDING, 'Pendiente'), (CANCELLED, 'Cancelada'),
-                    (PENDING_ADMIN, 'En revisión por Decanato'),
-                    (PENDING_CAI, 'En revisión por CAi'))
-
-    state = models.CharField(max_length=2,
-                             choices=VALID_STATES,
-                             default=PENDING_CAI,
-                             verbose_name='Estado de la solicitud')
-    name = models.CharField(max_length=200,
-                            verbose_name='Nombre de la actividad')
-    description = models.TextField(max_length=20000,
-                                   verbose_name='Descripción de la actividad')
-    space_1 = models.ForeignKey(Space,
-                                blank=True,
-                                null=True,
-                                on_delete=models.CASCADE,
-                                related_name='space_1')
-    space_2 = models.ForeignKey(Space,
-                                blank=True,
-                                null=True,
-                                on_delete=models.CASCADE,
-                                related_name='space_2')
-    space_3 = models.ForeignKey(Space,
-                                blank=True,
-                                null=True,
-                                on_delete=models.CASCADE,
-                                related_name='space_3')
-    equipment = models.ForeignKey(Equipment,
-                                  blank=True,
-                                  null=True,
-                                  on_delete=models.CASCADE)
-    date_start = models.DateTimeField(blank=False,
-                                      verbose_name='Fecha de inicio')
-    date_finish = models.DateTimeField(blank=False,
-                                       verbose_name='Fecha de término')
-    in_charge = models.EmailField(max_length=200,
-                                  verbose_name='Correo de encargado')
-    participants_amount = models.PositiveIntegerField(
-        default=0, verbose_name='Número de participantes')
-    admin_link = models.URLField(
-        blank=True, null=True, verbose_name='Link a administración del campus')
-    creator = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
     @staticmethod
     def format_time_calendar(datetime_instance):
         # 2019-09-16T16:00:00
-        return datetime_instance.strftime('%Y-%m-%dT%H:%M:%S')
+        return datetime_instance.strftime("%Y-%m-%dT%H:%M:%S")
 
     @property
     def activity_format_calendar(self):
         activities = []
         for space in [self.space_1, self.space_2, self.space_3]:
-            if space != '' and space is not None:
+            if space != "" and space is not None:
                 event = {
-                    'title': str(self.name),
-                    'start': Activity.format_time_calendar(self.date_start),
-                    'end': Activity.format_time_calendar(self.date_finish),
-                    'color': str(space.color)
+                    "title": str(self.name),
+                    "start": Activity.format_time_calendar(self.date_start),
+                    "end": Activity.format_time_calendar(self.date_finish),
+                    "color": str(space.color),
                 }
                 activities.append(event)
         return activities
@@ -124,17 +103,16 @@ class Activity(models.Model):
     def valid_spaces(self):
         spaces = [self.space_1, self.space_2, self.space_3]
         spaces = list(
-            map(lambda space: str(space),
-                filter(lambda space: space is not None, spaces)))
-        spaces = ', '.join(spaces)
+            map(
+                str,
+                filter(lambda space: space is not None, spaces),
+            )
+        )
+        spaces = ", ".join(spaces)
         return spaces
-
-    def __str__(self):
-        return self.name
 
 
 class Coordinator(models.Model):
-
     name = models.CharField(max_length=200)
     email = models.EmailField()
 
@@ -143,3 +121,6 @@ class Coordinator(models.Model):
     class Meta:
         verbose_name = "Coordinador CAi"
         verbose_name_plural = "Coordinador CAi (solo tener 1)"
+
+    def __str__(self):
+        return self.name
--- activities/views.py
+++ activities/views.py
@@ -1,32 +1,35 @@
 import json
-from activities.forms import ActivityForm, NotificationForm
-from activities.models import Activity, Space
-from django.db.models import Q
-from django.shortcuts import redirect
-from django.views import generic
-from django.urls import reverse_lazy
-from django.http import HttpResponse
-from django.template import loader
+from typing import ClassVar
+
+from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.contrib import messages
-from accounts.models import Profile
 from django.core.mail import send_mail
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
 from django.template.loader import render_to_string
+from django.urls import reverse_lazy
 from django.utils.html import strip_tags
-from django.conf import settings
+from django.views import generic
+
+from accounts.models import Profile
+from activities.forms import ActivityForm, NotificationForm
+from activities.models import Activity, Space
 
 from .models import Coordinator
 
-DEBUG = getattr(settings, 'DEBUG')
+DEBUG = settings.DEBUG
 ACTIVITIES_PER_PAGE = 20
 
 
 # Limits the permissions of viewing and modifying any of the activities that
 # the user is not the owner and that the user dos not have permission to view
-class PermissionMixin(object):
+class PermissionMixin:
     def get_object(self, *args, **kwargs):
-        obj = super(PermissionMixin, self).get_object(*args, **kwargs)
+        obj = super().get_object(*args, **kwargs)
         # staff check
         if self.request.user.is_staff:
             return obj
@@ -41,64 +44,60 @@ class FillInformation:
     def dispatch(self, request, *args, **kwargs):
         user = self.request.user
         profile = Profile.objects.get(user=user)
-        if profile.rut == '' or profile.phone == '' or profile.profile_type == '':
-            return redirect('update_profile', pk=user.id)
+        if profile.rut == "" or profile.phone == "" or profile.profile_type == "":
+            return redirect("update_profile", pk=user.id)
         else:
             return super().dispatch(request, *args, **kwargs)
 
 
-class NewActivity(LoginRequiredMixin, FillInformation, PermissionMixin,
-                  generic.CreateView):
-
+class NewActivity(LoginRequiredMixin, FillInformation, PermissionMixin, generic.CreateView):
     form_class = ActivityForm
-    page_name = 'new'
-    template_name = 'activities/activity_form.html'
-    success_url = reverse_lazy('activities_list')
+    page_name = "new"
+    template_name = "activities/activity_form.html"
+    success_url = reverse_lazy("activities_list")
 
     def get(self, request):
-        template = loader.get_template('activities/activity_form.html')
+        template = loader.get_template("activities/activity_form.html")
         form = ActivityForm(request.user)
-        required_links = [
-            name for name in Space.objects.filter(admin_required=True)
-        ]
+        required_links = list(Space.objects.filter(admin_required=True))
         return HttpResponse(
             template.render(
                 {
-                    'form': form,
-                    'required_links': required_links,
-                    'page_name': 'Nueva actividad',
-                    'button': 'Crear actividad'
-                }, request))
+                    "form": form,
+                    "required_links": required_links,
+                    "page_name": "Nueva actividad",
+                    "button": "Crear actividad",
+                },
+                request,
+            )
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['required_links'] = [
-            name for name in Space.objects.filter(admin_required=True)
-        ]
-        context['page_name'] = 'Nueva actividad'
-        context['button'] = 'Crear actividad'
+        context["required_links"] = list(Space.objects.filter(admin_required=True))
+        context["page_name"] = "Nueva actividad"
+        context["button"] = "Crear actividad"
         return context
 
 
 class ActivitiesList(LoginRequiredMixin, FillInformation, generic.ListView):
-
-    template_name = 'activities/activities_list.html'
+    template_name = "activities/activities_list.html"
     model = Activity
-    context_object_name = 'activities'
+    context_object_name = "activities"
     paginate_by = ACTIVITIES_PER_PAGE
 
     def get_queryset(self):
-        maker = (Q(creator=self.request.user))
-        parameter = self.request.GET.get('search')
+        maker = Q(creator=self.request.user)
+        parameter = self.request.GET.get("search")
 
-        if parameter != '' and parameter is not None:
+        if parameter != "" and parameter is not None:
             # searhch is by id
             if str.isnumeric(parameter):
                 search = Q(id=int(parameter))
 
             # search is by name
             if str.isalpha(parameter):
-                search = (Q(name__icontains=parameter))
+                search = Q(name__icontains=parameter)
 
             return Activity.objects.filter(search & maker).order_by("-id")
 
@@ -107,26 +106,25 @@ class ActivitiesList(LoginRequiredMixin, FillInformation, generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_name'] = 'Mis solicitudes'
+        context["page_name"] = "Mis solicitudes"
         return context
 
 
 class ActivitiesListStaff(ActivitiesList):
-
-    template_name = 'activities/activities_list_staff.html'
+    template_name = "activities/activities_list_staff.html"
 
     def get_queryset(self):
-        state = (Q(state='PC') | Q(state='PA'))
-        parameter = self.request.GET.get('search')
+        state = Q(state="PC") | Q(state="PA")
+        parameter = self.request.GET.get("search")
 
-        if parameter != '' and parameter is not None:
+        if parameter != "" and parameter is not None:
             # searhch is by id
             if str.isnumeric(parameter):
                 search = Q(id=int(parameter))
 
             # search is by name
             if str.isalpha(parameter):
-                search = (Q(name__icontains=parameter))
+                search = Q(name__icontains=parameter)
 
             return Activity.objects.filter(search & state).order_by("-id")
 
@@ -135,23 +133,23 @@ class ActivitiesListStaff(ActivitiesList):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url_search'] = 'pending'
-        context['page_name'] = 'Solicitudes pendientes'
+        context["url_search"] = "pending"
+        context["page_name"] = "Solicitudes pendientes"
         return context
 
 
 class ActivitiesListStaffAll(ActivitiesListStaff):
     def get_queryset(self):
-        parameter = self.request.GET.get('search')
+        parameter = self.request.GET.get("search")
 
-        if parameter != '' and parameter is not None:
+        if parameter != "" and parameter is not None:
             # searhch is by id
             if str.isnumeric(parameter):
                 search = Q(id=int(parameter))
 
             # search is by name
             if str.isalpha(parameter):
-                search = (Q(name__icontains=parameter))
+                search = Q(name__icontains=parameter)
 
             return Activity.objects.filter(search).order_by("-id")
 
@@ -160,68 +158,56 @@ class ActivitiesListStaffAll(ActivitiesListStaff):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url_search'] = 'all'
-        context['page_name'] = 'Todas las solicitudes'
+        context["url_search"] = "all"
+        context["page_name"] = "Todas las solicitudes"
         return context
 
 
-class ActivityDelete(LoginRequiredMixin, FillInformation, PermissionMixin,
-                     generic.DeleteView):
-
+class ActivityDelete(LoginRequiredMixin, FillInformation, PermissionMixin, generic.DeleteView):
     model = Activity
-    template_name = 'activities/activity_confirm_delete.html'
-    success_url = reverse_lazy('activities_list')
+    template_name = "activities/activity_confirm_delete.html"
+    success_url = reverse_lazy("activities_list")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creator'] = Profile.objects.get(
-            user=self.get_object().creator)
-        context['page_name'] = 'Eliminar actividad'
-        context['button'] = 'Eliminar'
+        context["creator"] = Profile.objects.get(user=self.get_object().creator)
+        context["page_name"] = "Eliminar actividad"
+        context["button"] = "Eliminar"
         return context
 
 
-class ActivityUpdate(LoginRequiredMixin, FillInformation, PermissionMixin,
-                     generic.UpdateView):
-
+class ActivityUpdate(LoginRequiredMixin, FillInformation, PermissionMixin, generic.UpdateView):
     model = Activity
     form_class = ActivityForm
-    page_name = 'updates'
-    template_name = 'activities/activity_form.html'
-    success_url = reverse_lazy('activities_list')
+    page_name = "updates"
+    template_name = "activities/activity_form.html"
+    success_url = reverse_lazy("activities_list")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creator'] = Profile.objects.get(
-            user=self.get_object().creator)
-        context['required_links'] = [
-            name for name in Space.objects.filter(admin_required=True)
-        ]
-        context['page_name'] = 'Actualizar actividad'
-        context['button'] = 'Actualizar actividad'
+        context["creator"] = Profile.objects.get(user=self.get_object().creator)
+        context["required_links"] = list(Space.objects.filter(admin_required=True))
+        context["page_name"] = "Actualizar actividad"
+        context["button"] = "Actualizar actividad"
         return context
 
 
-class ActivityDetail(LoginRequiredMixin, FillInformation, PermissionMixin,
-                     generic.DetailView):
-
+class ActivityDetail(LoginRequiredMixin, FillInformation, PermissionMixin, generic.DetailView):
     model = Activity
-    template_name = 'activities/detailed_view.html'
+    template_name = "activities/detailed_view.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creator'] = Profile.objects.get(
-            user=self.get_object().creator)
-        context['page_name'] = 'Detalle de la actividad'
+        context["creator"] = Profile.objects.get(user=self.get_object().creator)
+        context["page_name"] = "Detalle de la actividad"
         return context
 
 
-class ActivityChangeState(LoginRequiredMixin, FillInformation, PermissionMixin,
-                          generic.UpdateView):
+class ActivityChangeState(LoginRequiredMixin, FillInformation, PermissionMixin, generic.UpdateView):
     model = Activity
-    fields = ['state']
-    page_name = 'Cambio de estado de solicitud'
-    template_name = 'activities/activities_status_update_form.html'
+    fields: ClassVar[list[str]] = ["state"]
+    page_name = "Cambio de estado de solicitud"
+    template_name = "activities/activities_status_update_form.html"
 
     def send_email(self, activity, mailed, template):
         subject = f'Solicitud de espacios aprobada "{activity.name}"'
@@ -229,83 +215,84 @@ class ActivityChangeState(LoginRequiredMixin, FillInformation, PermissionMixin,
         coordinator = Coordinator.objects.all().first()
 
         html_content = render_to_string(
-            f'emails/{template}', {
-                'name': activity.name,
-                'start_date': activity.date_start,
-                'finish_date': activity.date_finish,
-                'place': activity.valid_spaces,
-                'creator': activity.creator,
-                'description': activity.description,
-                'participants_amount': activity.participants_amount,
-                'in_charge': activity.in_charge,
-                'coordinator': coordinator,
-            })
+            f"emails/{template}",
+            {
+                "name": activity.name,
+                "start_date": activity.date_start,
+                "finish_date": activity.date_finish,
+                "place": activity.valid_spaces,
+                "creator": activity.creator,
+                "description": activity.description,
+                "participants_amount": activity.participants_amount,
+                "in_charge": activity.in_charge,
+                "coordinator": coordinator,
+            },
+        )
         text_content = strip_tags(html_content)
 
-        send_mail(subject,
-                  text_content,
-                  'contacto@cai.cl', [mailed],
-                  fail_silently=False,
-                  html_message=html_content)
+        send_mail(
+            subject,
+            text_content,
+            "contacto@cai.cl",
+            [mailed],
+            fail_silently=False,
+            html_message=html_content,
+        )
 
     def get_success_url(self):
-        activity = Activity.objects.get(pk=self.kwargs['pk'])
+        activity = Activity.objects.get(pk=self.kwargs["pk"])
         # send autogenerated email to administration if current user
         # is part of the administration, so that the administration
         # is notified of the state of the activities
-        if self.request.user.profile.administrative and activity.state == 'A':
+        if self.request.user.profile.administrative and activity.state == "A":
             # send mail to administration and to person in charge
-            self.send_email(activity, 'administracion.csj@uc.cl',
-                            'administration_approved.html')
+            self.send_email(activity, "administracion.csj@uc.cl", "administration_approved.html")
             # TODO: preguntarle a jesuri si mail es solo con decanato
-            self.send_email(activity, activity.in_charge,
-                            'approved_activity.html')
+            self.send_email(activity, activity.in_charge, "approved_activity.html")
 
-        return reverse_lazy('activities_list_staff')
+        return reverse_lazy("activities_list_staff")
         # temporarily disabled the option of sendig emails directly
-        # return reverse_lazy('send_email', kwargs={'pk': self.kwargs['pk']})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creator'] = Profile.objects.get(
-            user=self.get_object().creator)
-        context['page_name'] = 'Cambio de estado de la solicitud'
-        context['button'] = 'Cambiar estado de actividad'
+        context["creator"] = Profile.objects.get(user=self.get_object().creator)
+        context["page_name"] = "Cambio de estado de la solicitud"
+        context["button"] = "Cambiar estado de actividad"
         return context
 
 
-class ActivityStatusNotification(LoginRequiredMixin, FillInformation,
-                                 PermissionMixin, generic.FormView):
-
+class ActivityStatusNotification(LoginRequiredMixin, FillInformation, PermissionMixin, generic.FormView):
     form_class = NotificationForm
     model = Activity
-    page_name = 'Notificar cambio de estado'
-    template_name = 'activities/notify_status_change.html'
+    page_name = "Notificar cambio de estado"
+    template_name = "activities/notify_status_change.html"
 
     def form_valid(self, form):
-        activity = Activity.objects.get(pk=self.kwargs['pk'])
-        form.send_email(form.data['subject'], form.data['body'],
-                        'contacto@caiuc.cl', [activity.in_charge])
-        messages.info(self.request,
-                      f'Se ha enviado un correo a {activity.in_charge}')
-        return redirect('activities_list_staff')
+        activity = Activity.objects.get(pk=self.kwargs["pk"])
+        form.send_email(
+            form.data["subject"],
+            form.data["body"],
+            "contacto@caiuc.cl",
+            [activity.in_charge],
+        )
+        messages.info(self.request, f"Se ha enviado un correo a {activity.in_charge}")
+        return redirect("activities_list_staff")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_name'] = 'Notificar cambio de estado'
-        context['button'] = 'Enviar correo'
+        context["page_name"] = "Notificar cambio de estado"
+        context["button"] = "Enviar correo"
         return context
 
 
 class Calendar(generic.TemplateView):
-
-    page_name = 'calendar'
-    template_name = 'activities/calendar.html'
+    page_name = "calendar"
+    template_name = "activities/calendar.html"
 
     def get(self, request):
-        template = loader.get_template('activities/calendar.html')
+        template = loader.get_template("activities/calendar.html")
         events = []
-        for activity in Activity.objects.filter(state='A'):
+        for activity in Activity.objects.filter(state="A"):
             events.extend(activity.activity_format_calendar)
         spaces = []
         for space in Space.objects.all():
@@ -313,29 +300,25 @@ class Calendar(generic.TemplateView):
         return HttpResponse(
             template.render(
                 {
-                    'events': json.dumps(events),
-                    'spaces': spaces,
-                    'page_name': 'Calendario de actividades'
-                }, request))
+                    "events": json.dumps(events),
+                    "spaces": spaces,
+                    "page_name": "Calendario de actividades",
+                },
+                request,
+            )
+        )
 
 
-class HomeWithCalendar(LoginRequiredMixin, FillInformation,
-                       generic.TemplateView):
-
-    page_name = 'home'
-    template_name = 'home.html'
+class HomeWithCalendar(LoginRequiredMixin, FillInformation, generic.TemplateView):
+    page_name = "home"
+    template_name = "home.html"
 
     def get(self, request):
-        template = loader.get_template('home.html')
+        template = loader.get_template("home.html")
         events = []
-        for activity in Activity.objects.filter(state='A',
-                                                creator=request.user):
+        for activity in Activity.objects.filter(state="A", creator=request.user):
             events.extend(activity.activity_format_calendar)
         spaces = []
         for space in Space.objects.all():
             spaces.append(space)
-        return HttpResponse(
-            template.render({
-                'events': json.dumps(events),
-                'spaces': spaces
-            }, request))
+        return HttpResponse(template.render({"events": json.dumps(events), "spaces": spaces}, request))
