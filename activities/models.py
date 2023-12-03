2def5b01907cfe22cfea8f4b95876193b881320c
olguitab <101607875+olguitab@users.noreply.github.com>
2023-12-03 19:46:43 -0300

    Update models.py (#38)

--- activities/models.py
+++ activities/models.py
@@ -96,7 +96,7 @@ class Activity(models.Model):
                     "title": str(self.name),
                     "start": Activity.format_time_calendar(self.date_start),
                     "end": Activity.format_time_calendar(self.date_finish),
-                    "space": space,
+                    "space": self.space_1,
                 }
                 activities.append(event)
         return activities
