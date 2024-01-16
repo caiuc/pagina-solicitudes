0e2af78398179577123f7a57f6e049a5ecf115a2
olguitab <101607875+olguitab@users.noreply.github.com>
2024-01-16 14:47:48 -0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("update/<int:pk>/", views.ProfileUpdate.as_view(), name="update_profile"),
+    path("tutorial/", views.TutorialView.as_view(),name="tutorial"),
 ]
